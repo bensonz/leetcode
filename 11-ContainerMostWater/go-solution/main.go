e6a45314bf1b534af9913c1058f4dd633e13ef59
bensonz <mr.bz@hotmail.com>
2019-05-16 17:10:57 +0800

    Added question 11, This might just be the most interesting question i've seen so far

---
+++ 11-ContainerMostWater/go-solution/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// solution idea
+/*
+  This question is pretty interesting
+
+
+*/
+func maxArea(height []int) int {
+
+}
+
+func main() {
+	fmt.Println()
+}
