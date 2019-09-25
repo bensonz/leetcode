6c1b62faed205270809f5cf40aaef9df35eee7c5
bensonz <mr.bz@hotmail.com>
2019-09-25 14:11:51 +0800

    added 5-longestPalindrome string

---
+++ 5-LongestPalSubstring/go-solution/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+obviously there's brute force, loop through it and find the length of every palindrome.
+*/
+
+func extendPalindrome(s string, i, j int) (length, start int) {
+	for i >= 0 && j < len(s) && s[i] == s[j] {
+		i--
+		j++
+	}
+	return j - i - 1, i + 1
+}
+
+func longestPalindrome(s string) string {
+	sLength := len(s)
+	if sLength < 2 {
+		return s
+	}
+	max := 0
+	start := 0
+	for i := 0; i < len(s); i++ {
+		lo, so := extendPalindrome(s, i, i)
+		if max < lo {
+			max = lo
+			start = so
+		}
+		le, se := extendPalindrome(s, i, i+1)
+		if max < le {
+			max = le
+			start = se
+		}
+	}
+	return s[start : start+max]
+}
+
+func main() {
+	l := longestPalindrome
+	fmt.Println(l(""))
+	fmt.Println(l("ac"))
+	fmt.Println(l("bb"))
+	fmt.Println(l("babad"))
+	fmt.Println(l("cbbddbb"))
+	fmt.Println(l("abcdeffedjklkjd"))
+}
