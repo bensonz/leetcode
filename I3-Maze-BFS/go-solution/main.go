a7281f3fe8cc899994c7359437e0426f1618fa6f
bensonz <mr.bz@hotmail.com>
2019-09-25 14:12:35 +0800

    bfs fixed

--- I3-Maze-BFS/go-solution/main.go
+++ I3-Maze-BFS/go-solution/main.go
@@ -29,10 +29,10 @@ var dirs = [][]int{
 	{-1, 0}, //up
 }
 
-func walk(maze [][]int, row, col int, solution *[][]bool) bool {
-	q := [][]int{{row, col}}     // initialize our queue
-	(*solution)[row][col] = true //mark first
-	for {                        // while queue not empty
+func walk(maze [][]int, row, col int) bool {
+	q := [][]int{{row, col}} // initialize our queue
+	solution := [][]int{}
+	for { // while queue not empty
 		if len(q) == 0 {
 			break
 		}
@@ -40,14 +40,14 @@ func walk(maze [][]int, row, col int, solution *[][]bool) bool {
 		curRow := q[0][0]
 		curCol := q[0][1]
 		q = q[1:]
-		(*solution)[curRow][curCol] = true
+		// add to solution
+		solution = append(solution, []int{curRow, curCol})
 		maze[curRow][curCol] = 2
 		// check condition
 		if isSolved(maze, curRow, curCol) {
 			return true
 		}
 		// add to action queue
-		deadend := 0
 		for _, dir := range dirs {
 			nr := curRow + dir[0]
 			nc := curCol + dir[1]
@@ -55,31 +55,19 @@ func walk(maze [][]int, row, col int, solution *[][]bool) bool {
 			if check(maze, nr, nc) {
 				q = append(q, []int{nr, nc})
 				// no need to unmark on BFS
-			} else {
-				deadend++
-				// fmt.Printf(" Deadend: %d,%d | %d \n", nr, nc, deadend)
 			}
 		}
-		if deadend == 4 {
-			fmt.Printf("Unmarking %d, %d \n", curRow, curCol)
-			(*solution)[curRow][curCol] = false
-		}
 	}
-	(*solution)[row][col] = false //unmark first
+	fmt.Println(solution)
 	return false
 }
 
-func solveMaze(maze [][]int) [][]bool {
-	maxRow := len(maze)
-	maxCol := len(maze[0])
-	solution := make([][]bool, maxRow)
-	for i := 0; i < maxRow; i++ {
-		solution[i] = make([]bool, maxCol)
-	}
-	if walk(maze, 0, 0, &solution) {
+func solveMaze(maze [][]int) bool {
+	if walk(maze, 0, 0) {
 		fmt.Println("Has solution.")
+		return true
 	}
-	return solution
+	return false
 }
 
 func main() {
@@ -92,7 +80,7 @@ func main() {
 		{1, 1, 0, 0, 0},
 	}
 	ans := solveMaze(maze)
-	for _, e := range ans {
-		fmt.Println(e)
-	}
+	// for _, e := range ans {
+	fmt.Println(ans)
+	// }
 }
