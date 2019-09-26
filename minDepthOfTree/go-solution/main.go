8710c917184a0c71d800cc2b80e66851e24a6a2f
bensonz <mr.bz@hotmail.com>
2019-09-27 00:55:00 +0800

    added a minimum depth of tree

---
+++ minDepthOfTree/go-solution/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func minDepth(root *TreeNode) int {
+	var q, newQ [](TreeNode) // keep track of the nodes
+	// put first node in
+	if root == nil {
+		return 0
+	}
+	q = append(q, *root)
+	height := 0
+	for {
+		// dequeue all
+		for _, node := range q {
+			if node.Left == nil && node.Right == nil {
+				// this node doesn't have anything
+				return height + 1
+			}
+			if node.Left != nil {
+				// has a left, put to queue
+				newQ = append(newQ, *node.Left)
+			}
+			if node.Right != nil {
+				// has a right
+				newQ = append(newQ, *node.Right)
+			}
+		}
+		height++
+		q = newQ
+		newQ = []TreeNode{}
+	}
+
+}
+func main() {
+	fmt.Println()
+}
