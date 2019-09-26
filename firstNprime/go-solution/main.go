2e7909c356619cafbae86626b1d028efb50441ed
bensonz <mr.bz@hotmail.com>
2019-09-27 00:54:45 +0800

    added a first n prime numbers

---
+++ firstNprime/go-solution/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fkprime(n int) []int {
+	var result []int
+	for len(result) < n {
+		result = append(slice, elems)
+	}
+}
+
+func main() {
+	fmt.Println(fkprime(n))
+}
